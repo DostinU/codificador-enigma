acf3791ac1e2b60ccd05f87fa82c5b93dfc80ef4
Carlos-Escobar-24001994 <24001994@galileo.edu>
2025-04-18 10:44:52 -0600

    setkey

--- codificador.py
+++ codificador.py
@@ -1,3 +1,46 @@
 print("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ Bienvenido al codificador !!!!!!!!!!!!!!!")
-print("¡¡¡¡¡¡¡¡ Creadores: Dostin Umaña Y Carlos Escobar!!!!!!!¡")
+print("Creadores: Dostin Umaña Y Carlos Escobar")
 print("Codificador Enigma")
+
+
+while True:
+    texto = input("codificador >> ").lower().strip()
+    print(texto)
+    if texto == "quit":
+        print("Saliendo ... \nGracias por usar nuestro codificador")
+        break
+    elif texto[0:7] == "setkey ":
+        llave = texto[7:len(texto)]
+        numeros = "0123456789"
+        hay_numero = False
+        for i in llave:
+            for n in numeros:
+                if i == n:
+                    hay_numero = True
+                    break
+        if hay_numero == False:
+            print("LLave Aceptada")
+        else:
+            print("ERROR! Expresión no valida") 
+     
+
+    elif texto[0:12] == "encode-text ":
+        print("Texto a cifrar")
+
+    elif texto[0:12] == "encode-file ":   
+        print("Archivo a cifrar")
+
+    elif texto[0:12] == "decode-text ":   
+        print("Texto a descifrar")
+
+    elif texto[0:12] == "decode-file ":   
+        print("Archivo a descifrar")
+
+    else:
+        print("ERROR! Expresión no valida")
+
+
+    
+
+
+
